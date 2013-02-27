215ea789e0b03858240236afed09d1a9bb1f7d26
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2013-02-27 12:28:39 +0100

    missing files ...

---
+++ CQT/Model/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CQT.Model
+{
+    class Sprite
+    {
+        public void Draw(SpriteBatch sb)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
