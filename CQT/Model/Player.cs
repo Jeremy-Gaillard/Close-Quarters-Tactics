2b31da786d6053573040444aa8875e559b63e3c0
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2013-03-10 16:40:57 +0100

    player stub

---
+++ CQT/Model/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CQT.Model;
+
+namespace CQT.Model
+{
+    class Player
+    {
+        protected String name;
+        // TODO : change sprite to character
+        protected Sprite character = null;
+
+        public Player(String name)
+        {
+            this.name = name;
+        }
+
+        public void setCharacter(Sprite character)
+        {
+            this.character = character;
+        }
+    }
+}
