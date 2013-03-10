7c1ff7edcde5b99134ec0f40a0b410beb8781943
mcaffart <mcaffart@gmail.com>
2013-03-10 17:26:22 +0100

    Fun fact. If you rename a file, be careful to add it again on git.

---
+++ CQT/Model/Map/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CQT.Model.Geometry;
+
+namespace CQT.Model.Map
+{
+    class Obstacle
+    {
+        public readonly Polyline polyline;
+        public readonly float height;
+
+        public Obstacle(Polyline _polyline, float _height)
+        {
+            //verify that the obstacle is closed
+            if(_polyline.lineList[0].p1.Equals(_polyline.lineList[_polyline.lineList.Count-1].p2))
+            {
+              polyline = _polyline;
+              height = _height;
+            }
+        }
+    }
+}
