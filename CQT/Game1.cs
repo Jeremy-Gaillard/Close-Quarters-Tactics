6266c7d3eff687fd78162ca7fb997770c6f82f28
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2013-04-03 17:13:08 +0200

    Network stub

--- CQT/Game1.cs
+++ CQT/Game1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -9,6 +10,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+
 using CQT.Model.Geometry;
 using CQT.Model.Map;
 using Geom = CQT.Model.Geometry;
