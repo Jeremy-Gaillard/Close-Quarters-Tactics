d264ae57f84bcb7080dfa69d48a5fd21c49e53e8
mcaffart <mcaffart@gmail.com>
2013-04-04 17:33:50 +0200

    Add a Main menu, need to be the main to run in order to work

--- CQT/Program.cs
+++ CQT/Program.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using CQT.View;
+using System.Threading;
 #endregion
 
-namespace CQT
+/*namespace CQT
 {
     /// <summary>
     /// The main class.
@@ -12,15 +14,21 @@ namespace CQT
     public static class Program
     {
         private static Game1 game;
+        public static bool close = false;
 
+        public Program()
+        {}
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        /*static void Main()
         {
             game = new Game1();
             game.Run();
-        }
+            
+            MainMenu menu = new MainMenu();
+            menu.Show();    
+        }*/
     }
-}
+}*/
---
+++ CQT/View/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CQT.View
+{
+    public partial class MainMenu : Form
+    {
+
+        public MainMenu()
+        {
+            InitializeComponent();
+            System.Console.Write("Hello world");
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new MainMenu());
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            System.Console.Write("Hello world");
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            //Begin a new game
+            Game1 game = new Game1();
+            game.Run();
+        }
+
+        private void bExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
