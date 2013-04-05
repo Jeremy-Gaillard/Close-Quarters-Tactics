b7835ab3826056705101a3e4f3186a268ac78fc8
mcaffart <mcaffart@gmail.com>
2013-04-05 19:10:13 +0200

    ajout de fichier resx

---
+++ CQT/View/MainMenu.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CQT.View
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bStart = new System.Windows.Forms.Button();
+            this.bJoin = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(165, 64);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "New Game";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bJoin
+            // 
+            this.bJoin.Location = new System.Drawing.Point(165, 134);
+            this.bJoin.Name = "bJoin";
+            this.bJoin.Size = new System.Drawing.Size(75, 23);
+            this.bJoin.TabIndex = 1;
+            this.bJoin.Text = "Join Game";
+            this.bJoin.UseVisualStyleBackColor = true;
+            // 
+            // bExit
+            // 
+            this.bExit.Location = new System.Drawing.Point(90, 211);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(75, 23);
+            this.bExit.TabIndex = 2;
+            this.bExit.Text = "Exit";
+            this.bExit.UseVisualStyleBackColor = true;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bJoin);
+            this.Controls.Add(this.bStart);
+            this.MaximizeBox = false;
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Interface Close Quarter Tactics";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bJoin;
+        private System.Windows.Forms.Button bExit;
+    }
+}
