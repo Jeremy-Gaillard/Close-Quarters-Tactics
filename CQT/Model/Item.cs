6a41824e35032bb98b99000cab87958bef06e305
mcaffart <mcaffart@gmail.com>
2013-04-05 19:01:04 +0200

    merge

--- CQT/Model/Item.cs
+++ CQT/Model/Item.cs
@@ -7,10 +7,16 @@ namespace CQT.Model
 {
 	public class Item : Entity
 	{
-		public Item (Texture2D _texture, Vector2 _position, Vector2 _size)
-			: base(_texture, _position, _size)
+		//public Item (Texture2D _texture, Vector2 _position, Vector2 _size)
+		//	: base(_texture, _position, _size)
+        public Item(Texture2D _texture, Vector2 _size)
+        	: base(_texture, _size)
 		{
 		}
+        public override Vector2 getPosition()
+        {
+            return new Vector2();
+        }
 	}
 }
 
