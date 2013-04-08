8d924546b79e4fb2b7f4a4d0650673715babc6d7
mcaffart <mcaffart@gmail.com>
2013-04-08 16:32:41 +0200

    little modification for obstacle, no need for the first point to be the last one in XML + a better map

--- CQT/Model/Map/Obstacle.cs
+++ CQT/Model/Map/Obstacle.cs
@@ -13,11 +13,17 @@ namespace CQT.Model.Map
 
         public Obstacle(Polyline _polyline, float _height)
         {
+            height = _height;
             //verify that the obstacle is closed
-            if(_polyline.lineList[0].p1.Equals(_polyline.lineList[_polyline.lineList.Count-1].p2))
+            if (_polyline.lineList[0].p1.Equals(_polyline.lineList[_polyline.lineList.Count - 1].p2))
             {
-              polyline = _polyline;
-              height = _height;
+                polyline = _polyline;
+            }
+            else
+            {
+                Line newLine = new Line(_polyline.lineList[_polyline.lineList.Count - 1].p2, _polyline.lineList[0].p1);
+                _polyline.lineList.Add(newLine);
+                polyline = _polyline;
             }
         }
     }
